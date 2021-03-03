0127da1b1e35dc4ae979e83b3c375173365d75e0
mrhpn <mr.htetphyonaing@gmail.com>
2021-03-03 19:05:16 +0630

    configured custom font

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,28 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
+	double xOffset, yOffset = 0;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			primaryStage.initStyle(StageStyle.UNDECORATED);
-			
+
 			Parent root = FXMLLoader.load(getClass().getResource("/resources/pages/Home.fxml"));
+
 			Scene scene = new Scene(root);
+
 			scene.getStylesheets().add(getClass().getResource("/resources/css/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
